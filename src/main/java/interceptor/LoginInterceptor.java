6d2787cac22638c2faa602cf6a6fe148e1957478
2969699014@qq.com <2969699014@qq.com>
2022-04-05 20:42:31 +0800

    Initial commit

---
+++ src/main/java/interceptor/LoginInterceptor.java
@@ -0,0 +1,11 @@
+package interceptor;
+
+/**
+ * @author XQL
+ * @version 1.0.0
+ * @ClassName LoginInterceptor.java
+ * @Description TODO
+ * @createTime 2022年04月05日 19:54:00
+ */
+public class LoginInterceptor {
+}
